fe6496edd578b81213cf80fd04187da87ad64ff3
Indrajeet Mothe <Indrajeet.Mothe@sscinc.com>
2024-03-22 13:03:47 +0530

    spring boot

---
+++ src/main/java/com/githubaction/gitaction/poc/Application.java
@@ -0,0 +1,22 @@
+package com.githubaction.gitaction.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	@GetMapping("/menu")
+	public String welcome() {
+		
+		return "Welcome to GitHub Action";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
